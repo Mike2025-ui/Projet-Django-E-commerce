a4f4ddd7c1e37ab2abef97bdb61250bd1b941760
Joseph-Michael <josephmichaelyaokonan25@gmail.com>
2025-06-24 12:02:02 +0200

    nouvel commit

--- projet1djan/urls.py
+++ projet1djan/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     # Include the URLs for the accounts app
     path('accounts/', include('accounts.urls')),
 ]
-# if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
